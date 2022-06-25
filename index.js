10ff13eb504b888c916b8eee7d39c0437edf5c8b
Pichunov-N <npichunov@gmail.com>
2022-06-25 20:53:11 +0300

    updated todolist

--- index.js
+++ index.js
@@ -4,28 +4,55 @@ const button = document.querySelector('button');
 const listNode = document.getElementById('list');
 
 const errorMessage = document.createElement('div');
+errorMessage.classList.add('error-text')
 button.after(errorMessage);
 
 form.onsubmit = (event) => {
     event.preventDefault();
     if (input.value === "") {
         errorMessage.innerHTML = 'Напишите что-нибудь';
+        errorMessage.style.marginBottom = '-18px'
         input.classList.add('error-colors')
         errorMessage.classList.add('error-colors')
         input.focus();
         return
-    } else errorMessage.innerHTML = '';
+    } else {
+        errorMessage.innerHTML = '';
+        errorMessage.style.marginBottom = '0px'
+    };
 
     const listItem = document.createElement('li');
     listItem.classList.add('li_item');
-    listItem.innerHTML = input.value;
+    // listItem.innerHTML = input.value;
+
+    const itemText = document.createElement('span')
+    itemText.classList.add('li_item__text')
+    itemText.innerHTML = input.value;
 
     const deleteItem = document.createElement('button');
-    deleteItem.classList.add('delete_li');
+    deleteItem.classList.add('li_item__delete');
     deleteItem.innerHTML = 'del';
 
+    const listItemCheckbox = document.createElement('input');
+    listItemCheckbox.type = 'checkbox';
+    listItemCheckbox.classList.add('li_item__checkbox')
+
+    listItemCheckbox.onclick = function (event) {
+        const checkedItem = event.target.className === 'li_item__checkbox'
+        if (checkedItem) {
+            const chosenItem = event.target.closest('.li_item');
+            chosenItem.style.textDecoration = 'line-through';
+            const chosenCheckbox = event.target.closest('.li_item__checkbox');
+            chosenCheckbox.setAttribute("disabled", "disabled");
+        }
+        console.log(event.target)
+    }
+
+
     listNode.append(listItem);
-    listItem.prepend(deleteItem);
+    listItem.append(itemText)
+    listItem.append(deleteItem);
+    listItem.prepend(listItemCheckbox)
 
     form.reset();
 }
@@ -35,7 +62,7 @@ input.oninput = function () {
 }
 
 listNode.addEventListener('click', (event) => {
-    const closeButton = event.target.className === 'delete_li';
+    const closeButton = event.target.className === 'li_item__delete';
     if (closeButton) {
         const liRow = event.target.closest('.li_item');
         liRow.remove();
